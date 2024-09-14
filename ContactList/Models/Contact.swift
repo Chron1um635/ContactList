3611c124b1bd194a4600416b81f02972802ee71b
Chron1um635 <chron1um522@gmail.com>
2024-09-14 12:15:57 +0300

    setup mode

--- ContactList/Models/Contact.swift
+++ ContactList/Models/Contact.swift
@@ -5,4 +5,47 @@
 //  Created by Максим Назаров on 13.09.2024.
 //
 
-import Foundation
+struct Contact {
+    let name: String
+    let surname: String
+    let phone: String
+    let email: String
+    var fullname: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getContacts() -> [Contact]{
+        let data = DataStore()
+        var contacts: [Contact] = []
+        let minCount = min(
+            data.phones.count,
+            data.surnames.count,
+            data.phones.count,
+            data.emails.count
+        )
+        for _ in 0..<minCount {
+            let name = data.names.remove(
+                at: Int.random(in: 0..<data.names.count)
+            )
+            let surname = data.surnames.remove(
+                at: Int.random(in: 0..<data.surnames.count)
+            )
+            let email = data.emails.remove(
+                at: Int.random(in: 0..<data.emails.count)
+            )
+            let phone = data.phones.remove(
+                at: Int.random(in: 0..<data.phones.count)
+            )
+            contacts.append(
+                Contact(
+                    name: name,
+                    surname: surname,
+                    phone: phone,
+                    email: email
+                )
+            )
+        }
+        
+        return contacts
+    }
+}
--- ContactList/Servises/DataStore.swift
+++ ContactList/Servises/DataStore.swift
@@ -5,4 +5,15 @@
 //  Created by Максим Назаров on 14.09.2024.
 //
 
-import Foundation
+class DataStore {
+    var names = ["Alex", "Max", "Sergey", "Vladimir", "Vladislav"]
+    var surnames = ["Murphy", "Jankin", "Williams", "Black", "Bulter"]
+    var phones = ["99999", "888888", "77777", "66666", "55555"]
+    var emails = [
+        "ads@das.ru",
+        "dfd@dasf.com",
+        "fsdf@dadf.ru",
+        "agdf@fdfg.com",
+        "fsmof@sfdg.com"
+    ]
+}
